a7cb4f6b0f5b00698718c24fd52f6c0c9a13ccb8
Shashi Raj <84275426+Shashi242@users.noreply.github.com>
2022-12-25 17:44:28 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,26 @@
 
+const contents = document.querySelectorAll('.content')
+const listItems = document.querySelectorAll('nav ul li')
+const bdys = document.querySelectorAll('.bdy');
+const clock = document.querySelector('.search');
+
+listItems.forEach((item, idx) => {
+    item.addEventListener('click', () => {
+        hideAllContents()
+        hideAllItems()
+
+        item.classList.add('active')
+        contents[idx].classList.add('show')
+        bdys[idx].classList.add('shw')
+    })
+})
+
+function hideAllContents() {
+    contents.forEach(content => content.classList.remove('show'))
+    bdys.forEach(bdy => bdy.classList.remove('shw'))
+}
+
+
+function hideAllItems() {
+    listItems.forEach(item => item.classList.remove('active'))
+}
